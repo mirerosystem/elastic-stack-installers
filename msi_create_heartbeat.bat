ca443557980601db2031d9db1b7d9c16d6b52a4a
최영헌 <younghun.choi@mirero.co.kr>
2020-07-01 17:37:39 +0900

    - Beat 별 msi 파일 생성하는 각 batch 파일 수정  (http://wish.mirero.co.kr/mirero/project/blue/1.0/h20-secfou-blu10-01/blue-cats/-/issues/588)

--- msi_create_heartbeat.bat
+++ msi_create_heartbeat.bat
@@ -1,10 +1,11 @@
 @echo off
-set DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-rem When built with Release Manager we put 
-rem Net Core SDK in the parent dir
-set PATH=%PATH%;%~dp0..\dotnet-sdk;
-
+if DEFINED  BEAT_VERSION (
+  echo Base BEAT_VERSION : '%BEAT_VERSION%'
+) else (
+  echo BEAT_VERSION not set. Set to '7.7.2-SNAPSHOT'
+  set BEAT_VERSION="7.7.2-SNAPSHOT"
+)
 pushd .
 cd %~dp0%
 dotnet run --project %~dp0src\installer\BeatPackageCompiler\BeatPackageCompiler.csproj -c Release --package="heartbeat-oss-7.7.2-SNAPSHOT-windows-x86"  --keep-temp-files --verbose
