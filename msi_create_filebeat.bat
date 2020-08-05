cc9fcaf1adf3ce20a0c93de0589bf2cf7053d348
DESKTOP-J3QJPGS\GDI_Test02 <d>
2020-08-05 19:35:44 +0900

    bat 파일 수정

--- msi_create_filebeat.bat
+++ msi_create_filebeat.bat
@@ -3,11 +3,11 @@
 if DEFINED  BEAT_VERSION (
   echo Base BEAT_VERSION : '%BEAT_VERSION%'
 ) else (
-  echo BEAT_VERSION not set. Set to '7.7.2-SNAPSHOT'
-  set BEAT_VERSION="7.7.2-SNAPSHOT"
+  echo BEAT_VERSION not set. Set to '7.8.2-SNAPSHOT'
+  set BEAT_VERSION="7.8.2-SNAPSHOT"
 )
 
 pushd .
 cd %~dp0%
-dotnet run --project %~dp0src\installer\BeatPackageCompiler\BeatPackageCompiler.csproj -c Release --package="filebeat-oss-%BEAT_VERSION%-windows-x86"  --keep-temp-files --verbose
+dotnet run --project %~dp0src\installer\BeatPackageCompiler\BeatPackageCompiler.csproj -c Release --package="filebeat-oss-7.8.2-windows-x86"  --keep-temp-files --verbose
 popd
