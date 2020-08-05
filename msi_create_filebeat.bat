d2dab50ec634b096cd542a28026b4b56a3e1cde6
DESKTOP-J3QJPGS\GDI_Test02 <d>
2020-08-05 19:32:35 +0900

    no message

--- msi_create_filebeat.bat
+++ msi_create_filebeat.bat
@@ -3,8 +3,8 @@
 if DEFINED  BEAT_VERSION (
   echo Base BEAT_VERSION : '%BEAT_VERSION%'
 ) else (
-  echo BEAT_VERSION not set. Set to '7.7.2-SNAPSHOT'
-  set BEAT_VERSION="7.7.2-SNAPSHOT"
+  echo BEAT_VERSION not set. Set to '7.8.2-SNAPSHOT'
+  set BEAT_VERSION="7.8.2-SNAPSHOT"
 )
 
 pushd .
